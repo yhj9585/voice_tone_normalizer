471ceafd47be70048ee7609b6327f70542a80975
yhj9585 <yhj6252@naver.com>
2024-04-16 20:52:20 +0900

    merge

--- audio-preprocessing.py
+++ audio-preprocessing.py
@@ -3,6 +3,8 @@
 Created on Tue Apr 16 14:47:49 2024
 
 @author: H.J. Yoon
+
+Pydub Load + Librosa Mel + MFCC >> PADDED DATA SAVE
 """
 
 import numpy as np
@@ -13,6 +15,8 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import os
 import subprocess
+
+import pickle
 
 # ffmpeg 설치 필요
 #AudioSegment.ffmpeg = "C:/Users/yhj62/scoop/apps/ffmpeg/7.0"
@@ -29,7 +33,51 @@ file3 = "train_data_01/003/114/114_003_0004.flac"
 output_folder = "output_test/"
 ult_src = "train_data_01/003/"
 
-def loadSoundperPerson(subFolder, audioBypeople) :
+# 임시로 몇 개 골라서 TEST
+folder_list = ['106','131','175']
+
+sample_rate = 16000
+n_fft = 400
+hop_length = 160
+
+pad2d = lambda a, i: a[:, 0:i] if a.shape[1] > i else np.hstack((a, np.zeros((a.shape[0], i-a.shape[1]))))
+
+def pydubTolibrosa(audioBypeople, folder, s) :
+    
+    sample = audioBypeople.get_array_of_samples()
+    arr = np.array(sample).astype(np.float32)       # TO librosa
+    y, index = librosa.effects.trim(arr)
+    
+    # Mel spectrogram
+    meled = librosa.feature.melspectrogram(y=y, sr=16000, n_mels=128)
+    meled_long = librosa.power_to_db(meled, ref=np.max)
+    
+    # Output Visualization
+    '''
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(meled_long, y_axis = 'mel', sr = 16000, hop_length = 16000, x_axis = 'time')
+    plt.colorbar(format = '%+2.0f dB')
+    plt.title('Mel Spectrogram')
+    plt.tight_layout()
+    plt.show()
+    '''
+    
+    # MFCC
+    mfcc_sound = librosa.feature.mfcc(S=meled_long, n_mfcc=100, n_fft=400, hop_length=160)
+    padded_mfcc = pad2d(mfcc_sound, 200)
+    
+    # 100*200 DATASET
+    #print(mfcc_sound.shape)
+    #print("MFCC SHAPE : ",padded_mfcc.shape)
+    #print(padded_mfcc)
+    
+    return padded_mfcc
+
+
+
+def loadSoundperPerson(folder, subFolder, audioBypeople) :
+    mfcc_data = []
+    
     audioBypeople = AudioSegment.empty()
     print(subFolder)
     
@@ -39,71 +87,56 @@ def loadSoundperPerson(subFolder, audioBypeople) :
         if nowfile.endswith('txt') :
             continue
         
-        if (s == 10) :return audioBypeople
-    
         thisaudio = os.path.join(subFolder, nowfile)
         print(thisaudio)
         
         sound = AudioSegment.from_file(thisaudio, format='flac')
-        sound = sound.set_frame_rate(44100).set_channels(1).set_sample_width(2)
+        sound = sound.set_frame_rate(16000).set_channels(1).set_sample_width(2)
         #plt.plot(sound.get_array_of_samples())
         
         audio_chunks = split_on_silence(sound,
             min_silence_len = 500,    # 최소 무음 길이 (밀리초 단위)
-            silence_thresh=-50,     # 무음으로 간주되는 dBFS 값
-            keep_silence = 50        # 분리된 오디오 조각들 간의 추가적인 무음 길이 (밀리초 단위)
-        )                           # 일정 이하의 소리는 자동으로 노이즈로 분류되어 제거
+            silence_thresh=-50,       # 무음으로 간주되는 dBFS 값
+            keep_silence = 50         # 분리된 오디오 조각들 간의 추가적인 무음 길이 (밀리초 단위)
+        )                             # 일정 이하의 소리는 자동으로 노이즈로 분류되어 제거
         
         print(len(audio_chunks))
         
         for c, chunks in enumerate(audio_chunks) :
             audioBypeople += chunks
-            plt.plot(audioBypeople.get_array_of_samples())
+            #plt.plot(audioBypeople.get_array_of_samples())
             print(chunks)
+            
+        mfcc_data.append(pydubTolibrosa(audioBypeople, folder, s))
+        audioBypeople = AudioSegment.empty()
 
-    return audioBypeople
+    print(mfcc_data)
+    print(len(mfcc_data))
+    
+    pickle_file = os.path.join(output_folder, f'pickle/{folder}_{0}.pickle')
+    pickle_folder = os.path.join(output_folder, f'pickle')
+    
+    if not os.path.exists(pickle_folder) :
+        os.makedirs(pickle_folder)
+    
+    with open(pickle_file, 'wb') as file :
+        pickle.dump(mfcc_data, file)
+    
+    return 
 
 
 all_list = os.listdir(ult_src)
-print(all_list)
+#print(all_list)
 #folder_list = [x for x in list if os.path.isdir(x)]
-folder_list = ['106']
 
 audioBypeople = AudioSegment.empty()
-
 
 for i, folder in enumerate(folder_list) :  # 폴더 전체에서 사람 찾기
     nowdir = os.path.join(ult_src, folder)
     print(nowdir)
-    audioBypeople = loadSoundperPerson(nowdir, audioBypeople)
+    loadSoundperPerson(folder, nowdir, audioBypeople)
     
+    #output = os.path.join(output_folder, f'test.wav')
+    #audioBypeople.export(output, format='wav')
     
-    output = os.path.join(output_folder, f'test.wav')
-    audioBypeople.export(output, format='wav')
-    
 
-
-
-
-
-# load audio file with Librosa
-#signal, sample_rate = librosa.load(file, sr=22050)
-
-
-# 노이즈 제거
-
-
-#testchunk = AudioSegment.empty()
-
-'''
-for i, chunks in enumerate(audio_chunks) :     
-    output_file = os.path.join(output_folder, f'chunk_{i}.wav')
-    chunks.export(output_file, format='wav')
-    testchunk += chunks
-    
-    print(chunks)
-
-
-
-
-'''
--- mfcc-kmeans.py
+++ mfcc-kmeans.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Apr 16 14:47:49 2024
 
-@author: yhj62
+@author: H.J. Yoon
 """
 
 import numpy as np
@@ -17,39 +17,85 @@ import subprocess
 # ffmpeg 설치 필요
 #AudioSegment.ffmpeg = "C:/Users/yhj62/scoop/apps/ffmpeg/7.0"
 
-
+####################################################################
+## TEST DATA ###
 file = "test2.flac"
 file2 = "short1.wav"
 
 file3 = "train_data_01/003/114/114_003_0004.flac"
 # 동일 사람 : 동일 폴더에 저장되어 있음
+######################################################################
 
+output_folder = "output_test/"
 ult_src = "train_data_01/003/"
+
+def loadSoundperPerson(subFolder, audioBypeople) :
+    audioBypeople = AudioSegment.empty()
+    print(subFolder)
+    
+    sublist = os.listdir(subFolder)
+
+    for s, nowfile in enumerate(sublist) : # 사람 폴더에서 파일 불러오기
+        if nowfile.endswith('txt') :
+            continue
+        
+        if (s == 10) :return audioBypeople
+    
+        thisaudio = os.path.join(subFolder, nowfile)
+        print(thisaudio)
+        
+        sound = AudioSegment.from_file(thisaudio, format='flac')
+        sound = sound.set_frame_rate(44100).set_channels(1).set_sample_width(2)
+        #plt.plot(sound.get_array_of_samples())
+        
+        audio_chunks = split_on_silence(sound,
+            min_silence_len = 500,    # 최소 무음 길이 (밀리초 단위)
+            silence_thresh=-50,     # 무음으로 간주되는 dBFS 값
+            keep_silence = 50        # 분리된 오디오 조각들 간의 추가적인 무음 길이 (밀리초 단위)
+        )                           # 일정 이하의 소리는 자동으로 노이즈로 분류되어 제거
+        
+        print(len(audio_chunks))
+        
+        for c, chunks in enumerate(audio_chunks) :
+            audioBypeople += chunks
+            plt.plot(audioBypeople.get_array_of_samples())
+            print(chunks)
+
+    return audioBypeople
+
+
+all_list = os.listdir(ult_src)
+print(all_list)
+#folder_list = [x for x in list if os.path.isdir(x)]
+folder_list = ['106']
+
+audioBypeople = AudioSegment.empty()
+
+
+for i, folder in enumerate(folder_list) :  # 폴더 전체에서 사람 찾기
+    nowdir = os.path.join(ult_src, folder)
+    print(nowdir)
+    audioBypeople = loadSoundperPerson(nowdir, audioBypeople)
+    
+    
+    output = os.path.join(output_folder, f'test.wav')
+    audioBypeople.export(output, format='wav')
+    
+
 
 
 
 
 # load audio file with Librosa
-signal, sample_rate = librosa.load(file, sr=22050)
+#signal, sample_rate = librosa.load(file, sr=22050)
 
 
-sound = AudioSegment.from_file(file3, format='flac')
-sound = sound.set_frame_rate(44100).set_channels(1).set_sample_width(2)
+# 노이즈 제거
 
 
-audio_chunks = split_on_silence(sound,
-    min_silence_len=500, # 최소 무음 길이 (밀리초 단위)
-    silence_thresh=-35, # 무음으로 간주되는 dBFS 값
-    keep_silence=100 # 분리된 오디오 조각들 간의 추가적인 무음 길이 (밀리초 단위)
-)
+#testchunk = AudioSegment.empty()
 
-
-output_folder = "output_test/"
-
-testchunk = AudioSegment.empty()
-
-#testchunk;
-
+'''
 for i, chunks in enumerate(audio_chunks) :     
     output_file = os.path.join(output_folder, f'chunk_{i}.wav')
     chunks.export(output_file, format='wav')
@@ -57,6 +103,7 @@ for i, chunks in enumerate(audio_chunks) :
     
     print(chunks)
 
-output = os.path.join(output_folder, f'test.wav')
-testchunk.export(output, format='wav')
 
+
+
+'''
